813b668d5442f84c1fb3699966ce9ba8f2cc4341
Aaron Lu <aaronnoh0409@gmail.com>
2022-02-16 21:24:51 -0500

    updated home page, added a img for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
+import Cool from '../components/ImgLib/cool.jpg'
+import './style.css'
+
 function Home() {
-    return(
+    return (
         <div>
-            <h2>Welcome to Ayeron's page</h2>
+            <div>
+                <h2>Welcome to Ayeron's page</h2>
+                <img src={Cool} alt="aRandomCoolPic" />
+            </div>
         </div>
     )
 }
